b21dcf0a240e727ef533281de4ff9361fdb3e1d8
Chaltear <alexispiget@yahoo.fr>
2024-11-03 17:20:56 +0100

    Add files via upload

---
+++ Application gestion matos/WindowsFormsAppGM13/WindowsFormsAppGM13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppGM13
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (LoginForm loginForm = new LoginForm())
+            {
+                if (loginForm.ShowDialog() == DialogResult.OK)
+                {
+                    Application.Run(new FormGenerale());
+                }
+                else
+                {
+                    MessageBox.Show("Connexion annulée.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
